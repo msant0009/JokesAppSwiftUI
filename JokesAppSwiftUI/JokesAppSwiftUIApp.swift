3bc87bf381493b56e7c98618f499837f07571dac
Mark Santoro <mark_santoro@comcast.net>
2024-09-02 10:01:52 -0700

    Initial Commit

---
+++ JokesAppSwiftUI/JokesAppSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  JokesAppSwiftUIApp.swift
+//  JokesAppSwiftUI
+//
+//  Created by Mark Santoro on 9/2/24.
+//
+
+import SwiftUI
+
+@main
+struct JokesAppSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
